f7ef63b76f4185fb2fdab33b636588c3350fe148
Bukyb <bukolaabayomi28@gmail.com>
2022-12-08 15:44:25 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**                                                                                                                                     
  * main - Entry Point
  *
@@ -10,4 +11,5 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
 }
