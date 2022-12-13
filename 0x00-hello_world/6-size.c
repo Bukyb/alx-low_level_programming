6c27704361962ab72534926d094d010c0bd966ed
Bukyb <bukolaabayomi28@gmail.com>
2022-12-13 07:11:55 -0800

    size is not

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,14 +4,15 @@
  * main - Entry
  * Return: Always 0
  */
-
 int main(void)
 {
+
 	char c;
 	int i;
 	long int I;
 	long long int II;
 	float f;
+
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(i));
@@ -19,4 +20,5 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(II));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
 	return (0);
+
 }
